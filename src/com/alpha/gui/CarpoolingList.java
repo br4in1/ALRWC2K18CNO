f6427bd11bcede1e7e0102a9f1966ca52ad021e8
Sofien Derbel <sofien.derbel@esprit.tn>
2018-05-04 11:59:00 +0100

    a

---
+++ src/com/alpha/gui/CarpoolingList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alpha.gui;
+
+import com.codename1.ui.util.Resources;
+
+/**
+ *
+ * @author Sof
+ */
+public class CarpoolingList extends BaseForm {
+    
+    public CarpoolingList(Resources res)
+    {
+    
+    }
+    
+}
